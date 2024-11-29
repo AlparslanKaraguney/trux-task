e92592d60620ee7a8892c4a2f05d7c19add9430b
Alparslan Karagüney <alparslan.karaguney@tga.gov.tr>
2024-11-29 19:08:18 +0300

    refactor: cleanup tests, coverage script, and Docker configuration
    - **`main_test.go`**:
      - Removed commented-out test functions (`TestStartHTTPServer`, `TestStartGRPCServer`) to streamline the test file and reduce clutter.
    
    - **`coverage.sh`**:
      - Eliminated commented-out code for enforcing a minimum coverage threshold. Simplified the script to generate and display coverage.
    
    - **`docker-compose.yml`**:
      - Adjusted spacing in the `healthcheck` test command for consistency.
      - Refactored commented-out `volumes` section to follow a consistent format.
    
    - Improved maintainability by removing unused and commented-out code.
    - Simplified configuration files for clarity and uniformity.
    - Streamlined scripts and tests, focusing on current requirements without unnecessary overhead.

--- coverage.sh
+++ coverage.sh
@@ -17,15 +17,3 @@ echo "Total Coverage: $COVERAGE"
 
 go tool cover -html=coverage.out -o coverage.html
 
-# Set required coverage threshold
-# REQUIRED_COVERAGE=70.0
-
-# # Use awk for floating-point comparison
-# awk -v cov="$COVERAGE" -v req="$REQUIRED_COVERAGE" 'BEGIN {
-#     if (cov+0 < req+0) {
-#         printf "Coverage is below %.1f%%. Current coverage: %.1f%%\n", req, cov;
-#         exit 1;
-#     } else {
-#         printf "Coverage meets the required threshold: %.1f%%\n", cov;
-#     }
-# }'
